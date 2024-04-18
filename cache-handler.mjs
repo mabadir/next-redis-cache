a54c60a1e7429341af69dfef7630e215e6aa497d
Mina Abadir <mina@abadir.email>
2024-04-18 10:27:19 -0400

    new handler

---
+++ cache-handler.mjs
@@ -0,0 +1,32 @@
+import { CacheHandler } from "@neshca/cache-handler";
+import createLruHandler from "@neshca/cache-handler/local-lru";
+// import createRedisHandler from "@neshca/cache-handler/redis-stack";
+// or if you are using Redis without the RedisJSON module
+import createRedisHandler from "@neshca/cache-handler/redis-strings";
+import { createClient } from "redis";
+
+CacheHandler.onCreation(async () => {
+  // always create a Redis client inside the `onCreation` callback
+  const client = createClient({
+    url: process.env.REDIS_URL ?? "redis://localhost:6379",
+  });
+
+  client.on("error", () => {});
+
+  await client.connect();
+
+  const redisHandler = createRedisHandler({
+    client,
+    // timeout for the Redis client operations like `get` and `set`
+    // after this timeout, the operation will be considered failed and the `localHandler` will be used
+    timeoutMs: 5000,
+  });
+
+  const localHandler = createLruHandler();
+
+  return {
+    handlers: [redisHandler, localHandler],
+  };
+});
+
+export default CacheHandler;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  cacheHandler:
-    process.env.NODE_ENV === "production"
-      ? require.resolve("./cache-handler.js")
-      : undefined,
+  cacheHandler: require.resolve("./cache-handler.mjs"),
   env: {
     NEXT_PUBLIC_REDIS_INSIGHT_URL:
       process.env.REDIS_INSIGHT_URL ?? "http://localhost:8001",
