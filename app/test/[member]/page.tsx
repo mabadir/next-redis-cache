903d7d64dc688dd63d2339ffe99c9784c1a00cd7
Mina Abadir <mina@abadir.email>
2024-04-18 21:00:41 -0400

    add fromContainerMetadata

--- app/test/[member]/page.tsx
+++ app/test/[member]/page.tsx
@@ -1,7 +1,11 @@
 import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { fromContainerMetadata } from "@aws-sdk/credential-providers";
 
 export async function generateStaticParams() {
-  const dynamo = new DynamoDBClient({ region: "us-east-2" });
+  const dynamo = new DynamoDBClient({
+    region: "us-east-2",
+    credentials: fromContainerMetadata(),
+  });
   const cmd = new ScanCommand({ TableName: "build" });
   const response = await dynamo.send(cmd);
   return response?.Items?.map((item) => item.PK) ?? [];
